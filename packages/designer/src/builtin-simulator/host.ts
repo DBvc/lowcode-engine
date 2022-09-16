digiforce-cloud/dvd-engine

feat: support for NotFoundComponent design state is optional

packages/designer/src/builtin-simulator/host.ts
@@ -232,6 +232,10 @@ export class BuiltinSimulatorHost implements ISimulatorHost<BuiltinSimulatorProp
     return engineConfig.get('thisRequiredInJSE') ?? true;
   }
 
+  get enableStrictNotFoundTip(): any {
+    return engineConfig.get('enableStrictNotFoundTip') ?? false;
+  }
+
   @computed get componentsAsset(): Asset | undefined {
     return this.get('componentsAsset');
   }

packages/renderer-core/src/renderer/base.tsx
@@ -104,13 +104,7 @@ export default function baseRendererFactory(): IBaseRenderComponent {
     return customBaseRenderer;
   }
 
-  const runtime = adapter.getRuntime();
-  const Component = runtime.Component as IGeneralConstructor<
-    IBaseRendererProps,
-    Record<string, any>,
-    any
-  >;
-  const { createElement } = runtime;
+  const { Component, createElement } = adapter.getRuntime();
   const Div = divFactory();
   const VisualDom = visualDomFactory();
   const AppContext = contextFactory();
@@ -125,7 +119,7 @@ export default function baseRendererFactory(): IBaseRenderComponent {
   const DEFAULT_LOOP_ARG_INDEX = 'index';
   let scopeIdx = 0;
 
-  return class BaseRenderer extends Component {
+  return class BaseRenderer extends Component<IBaseRendererProps, Record<string, any>> {
     static displayName = 'base-renderer';
 
     static defaultProps = {
@@ -533,6 +527,10 @@ export default function baseRendererFactory(): IBaseRenderComponent {
             {
               componentName: schema.componentName,
               componentId: schema.id,
+              enableStrictNotFoundTip: engine.props.enableStrictNotFoundTip,
+              ref: (ref: any) => {
+                ref && engine.props?.onCompGetRef(schema, ref);
+              },
             },
             this.__getSchemaChildrenVirtualDom(schema, scope, Comp),
           );

packages/renderer-core/src/renderer/renderer.tsx
@@ -4,14 +4,11 @@ import contextFactory from '../context';
 import { isFileSchema, isEmpty } from '../utils';
 import baseRendererFactory from './base';
 import divFactory from '../components/Div';
-import { IGeneralConstructor, IRenderComponent, IRendererProps, IRendererState } from '../types';
-import { RootSchema } from '@alilc/lowcode-types';
+import { IRenderComponent, IRendererProps, IRendererState } from '../types';
+import { NodeSchema, RootSchema } from '@alilc/lowcode-types';
 
 export default function rendererFactory(): IRenderComponent {
-  const runtime = adapter.getRuntime();
-  const Component = runtime.Component as IGeneralConstructor<IRendererProps, Record<string, any>>;
-  const PureComponent = runtime.PureComponent as IGeneralConstructor<IRendererProps, Record<string, any>>;
-  const { createElement, findDOMNode } = runtime;
+  const { PureComponent, Component, createElement, findDOMNode } = adapter.getRuntime();
   const RENDERER_COMPS: any = adapter.getRenderers();
   const BaseRenderer = baseRendererFactory();
   const AppContext = contextFactory();
@@ -21,7 +18,7 @@ export default function rendererFactory(): IRenderComponent {
 
   const debug = Debug('renderer:entry');
 
-  class FaultComponent extends PureComponent {
+  class FaultComponent extends PureComponent<NodeSchema> {
     render() {
       // FIXME: errorlog
       console.error('render error', this.props);
@@ -35,17 +32,22 @@ export default function rendererFactory(): IRenderComponent {
           color: '#ff0000',
           border: '2px solid #ff0000',
         },
-      }, '组件渲染异常，请查看控制台日志');
+      }, `${this.props.componentName || ''} 组件渲染异常，请查看控制台日志`);
     }
   }
 
-  class NotFoundComponent extends PureComponent {
+  class NotFoundComponent extends PureComponent<{
+    componentName: string;
+  } & IRendererProps> {
     render() {
-      return createElement(Div, this.props, this.props.children || 'Component Not Found');
+      if (this.props.enableStrictNotFoundTip) {
+        return `${this.props.componentName || ''} Component Not Found`;
+      }
+      return createElement(Div, this.props, this.props.children || `${this.props.componentName || ''} Component Not Found`);
     }
   }
 
-  return class Renderer extends Component {
+  return class Renderer extends Component<IRendererProps> {
     static dislayName = 'renderer';
 
     state: Partial<IRendererState> = {};

packages/renderer-core/src/types/index.ts
@@ -21,12 +21,12 @@ interface IGeneralComponent<P = {}, S = {}, SS = any> extends ComponentLifecycle
 }
 
 export type IGeneralConstructor<
-  P = {
+  T = {
     [key: string]: any;
   }, S = {
     [key: string]: any;
-  }, SS = any
-> = new (props: any, context: any) => IGeneralComponent<P, S, SS>;
+  }, D = any
+> = new <TT = T, SS = S, DD = D>(props: TT, context: any) => IGeneralComponent<TT, SS, DD>;
 
 /**
  * duck-typed History
@@ -133,6 +133,11 @@ export interface IRendererProps {
    * JSExpression 是否只支持使用 this 来访问上下文变量
    */
   thisRequiredInJSE?: boolean;
+  /**
+   * @default false
+   * 当组件找不到时开启严格提示模式，有子元素时不渲染
+   */
+  enableStrictNotFoundTip?: boolean;
 }
 
 export interface IRendererState {

DBvc/lowcode-engine

packages/designer/src/builtin-simulator/host.ts
@@ -232,6 +232,10 @@ export class BuiltinSimulatorHost implements ISimulatorHost<BuiltinSimulatorProp
     return engineConfig.get('thisRequiredInJSE') ?? true;
   }
 
+  get enableStrictNotFoundMode(): any {
+    return engineConfig.get('enableStrictNotFoundMode') ?? false;
+  }
+
   @computed get componentsAsset(): Asset | undefined {
     return this.get('componentsAsset');
   }

packages/renderer-core/src/renderer/base.tsx
@@ -104,13 +104,7 @@ export default function baseRendererFactory(): IBaseRenderComponent {
     return customBaseRenderer;
   }
 
-  const runtime = adapter.getRuntime();
-  const Component = runtime.Component as IGeneralConstructor<
-    IBaseRendererProps,
-    Record<string, any>,
-    any
-  >;
-  const { createElement } = runtime;
+  const { Component, createElement } = adapter.getRuntime();
   const Div = divFactory();
   const VisualDom = visualDomFactory();
   const AppContext = contextFactory();
@@ -125,7 +119,7 @@ export default function baseRendererFactory(): IBaseRenderComponent {
   const DEFAULT_LOOP_ARG_INDEX = 'index';
   let scopeIdx = 0;
 
-  return class BaseRenderer extends Component {
+  return class BaseRenderer extends Component<IBaseRendererProps, Record<string, any>> {
     static displayName = 'base-renderer';
 
     static defaultProps = {
@@ -533,6 +527,10 @@ export default function baseRendererFactory(): IBaseRenderComponent {
             {
               componentName: schema.componentName,
               componentId: schema.id,
+              enableStrictNotFoundMode: engine.props.enableStrictNotFoundMode,
+              ref: (ref: any) => {
+                ref && engine.props?.onCompGetRef(schema, ref);
+              },
             },
             this.__getSchemaChildrenVirtualDom(schema, scope, Comp),
           );

packages/renderer-core/src/renderer/renderer.tsx
@@ -4,14 +4,11 @@ import contextFactory from '../context';
 import { isFileSchema, isEmpty } from '../utils';
 import baseRendererFactory from './base';
 import divFactory from '../components/Div';
-import { IGeneralConstructor, IRenderComponent, IRendererProps, IRendererState } from '../types';
-import { RootSchema } from '@alilc/lowcode-types';
+import { IRenderComponent, IRendererProps, IRendererState } from '../types';
+import { NodeSchema, RootSchema } from '@alilc/lowcode-types';
 
 export default function rendererFactory(): IRenderComponent {
-  const runtime = adapter.getRuntime();
-  const Component = runtime.Component as IGeneralConstructor<IRendererProps, Record<string, any>>;
-  const PureComponent = runtime.PureComponent as IGeneralConstructor<IRendererProps, Record<string, any>>;
-  const { createElement, findDOMNode } = runtime;
+  const { PureComponent, Component, createElement, findDOMNode } = adapter.getRuntime();
   const RENDERER_COMPS: any = adapter.getRenderers();
   const BaseRenderer = baseRendererFactory();
   const AppContext = contextFactory();
@@ -21,7 +18,7 @@ export default function rendererFactory(): IRenderComponent {
 
   const debug = Debug('renderer:entry');
 
-  class FaultComponent extends PureComponent {
+  class FaultComponent extends PureComponent<NodeSchema> {
     render() {
       // FIXME: errorlog
       console.error('render error', this.props);
@@ -35,17 +32,22 @@ export default function rendererFactory(): IRenderComponent {
           color: '#ff0000',
           border: '2px solid #ff0000',
         },
-      }, '组件渲染异常，请查看控制台日志');
+      }, `${this.props.componentName || ''} 组件渲染异常，请查看控制台日志`);
     }
   }
 
-  class NotFoundComponent extends PureComponent {
+  class NotFoundComponent extends PureComponent<{
+    componentName: string;
+  } & IRendererProps> {
     render() {
-      return createElement(Div, this.props, this.props.children || 'Component Not Found');
+      if (this.props.enableStrictNotFoundMode) {
+        return `${this.props.componentName || ''} Component Not Found`;
+      }
+      return createElement(Div, this.props, this.props.children || `${this.props.componentName || ''} Component Not Found`);
     }
   }
 
-  return class Renderer extends Component {
+  return class Renderer extends Component<IRendererProps> {
     static dislayName = 'renderer';
 
     state: Partial<IRendererState> = {};

packages/renderer-core/src/types/index.ts
@@ -21,12 +21,12 @@ interface IGeneralComponent<P = {}, S = {}, SS = any> extends ComponentLifecycle
 }
 
 export type IGeneralConstructor<
-  P = {
+  T = {
     [key: string]: any;
   }, S = {
     [key: string]: any;
-  }, SS = any
-> = new (props: any, context: any) => IGeneralComponent<P, S, SS>;
+  }, D = any
+> = new <TT = T, SS = S, DD = D>(props: TT, context: any) => IGeneralComponent<TT, SS, DD>;
 
 /**
  * duck-typed History
@@ -133,6 +133,11 @@ export interface IRendererProps {
    * JSExpression 是否只支持使用 this 来访问上下文变量
    */
   thisRequiredInJSE?: boolean;
+  /**
+   * @default false
+   * 当组件找不到时开启严格提示模式，有子元素时不渲染
+   */
+  enableStrictNotFoundMode?: boolean;
 }
 
 export interface IRendererState {